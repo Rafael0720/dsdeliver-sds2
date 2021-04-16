636607e4b88fd195efbc89f60553d062cbedb3ea
Rafael Godoy <rafael.dgodoy@gmail.com>
2021-04-15 23:18:08 -0300

    Settings

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -5,14 +5,14 @@ const API_URL = process.env.REACT_APP_API_URL;
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export function fetchProducts() {
-    return axios(`${API_URL}/products`)
+    return axios(`${API_URL}/products`);
 }
 
 export function fetchLocalMapBox(local: string){
-    return axios (`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`)
+    return axios (`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`);
 }
 
 export function saveOrder(payload: OrderPayload) {
-    return axios.post(`${API_URL}/orders`, payload)
+    return axios.post(`${API_URL}/orders`, payload);
 }
 
